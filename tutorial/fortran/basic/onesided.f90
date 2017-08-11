430bf7b9a7249b8bc0c397091220944e8a24ec09
Benson Muite <bkmgit@users.noreply.github.com>
2017-08-12 01:46:17 +0300

    Update onesided.f90
    Fixed bugs

--- tutorial/fortran/basic/onesided.f90
+++ tutorial/fortran/basic/onesided.f90
@@ -5,14 +5,16 @@ subroutine wait_or_die(segment_id, notification_id, expected)
 
   integer(gaspi_notification_id_t), intent(inout) :: notification_id
   integer(gaspi_segment_id_t), intent(in) :: segment_id
-  integer(gaspi_notification_t), intent(inout) :: expected
+  integer(gaspi_notification_t), intent(in) :: expected
   integer(gaspi_notification_id_t) :: id
   integer(gaspi_return_t) :: res
   integer(gaspi_notification_t) :: val
 
   res = gaspi_notify_waitsome(segment_id, notification_id,1, id, GASPI_BLOCK)
+  call stop_on_error(res,"gaspi_notify_waitsome")
   res = gaspi_notify_reset(segment_id, id, val)
-
+  call stop_on_error(res,"gaspi_notify_reset")
+  if (val .ne. expected) call stop_on_error(-1,"did not get expected value")
 end subroutine wait_or_die
 
 
@@ -21,7 +23,6 @@ subroutine wait_for_queue_entries(queue, wanted_entries)
   use , intrinsic :: ISO_C_BINDING
   implicit none
 
-  ! queue should be an array, check how done
   integer(4), intent(in) :: wanted_entries
   integer(gaspi_queue_id_t), intent(inout) :: queue
   integer(gaspi_number_t) :: queue_size_max
@@ -30,13 +31,16 @@ subroutine wait_for_queue_entries(queue, wanted_entries)
   integer(gaspi_return_t) :: res
 
   res = gaspi_queue_size_max(queue_size_max)
+  call stop_on_error(res,"gaspi_queue_size_max")
   res = gaspi_queue_size(queue,queue_size)
+  call stop_on_error(res,"gaspi_queue_size")
   res = gaspi_queue_num(queue_num)
-
-  !if ( (queue_size + wanted_entries) .LEQ. queue_size_max)
+  call stop_on_error(res,"gaspi_queue_num")
+  if ( (queue_size + wanted_entries) .gt. queue_size_max) then
     queue = modulo(queue +1,queue_num)
     res = gaspi_wait(queue, GASPI_BLOCK)
-  !end if
+    call stop_on_error(res,"gaspi_wait")
+  endif
 
 end subroutine wait_for_queue_entries
 
@@ -72,11 +76,12 @@ subroutine wait_for_flush_queues()
   integer(gaspi_return_t) :: res
 
   res = gaspi_queue_num(queue_num)
-
+  call stop_on_error(res,"gaspi_queue_num")
   do
-    if ( queue >= queue_num) exit
     res = gaspi_wait(queue, GASPI_BLOCK)
+    call stop_on_error(res,"gaspi_wait")
     queue = queue + 1
+    if ( queue >= queue_num) exit
   end do
 
 end subroutine wait_for_flush_queues
@@ -108,56 +113,77 @@ program onesided
  integer(gaspi_rank_t) :: iProc, nProc, rProc, lProc
  integer(gaspi_timeout_t) :: timeout
  integer(gaspi_alloc_t) :: segment_alloc_policy 
- integer(gaspi_segment_id_t), parameter :: segment_id = 0
- integer(gaspi_size_t), parameter :: segment_size = 2*VLEN*8, write_size = VLEN*8
- integer(gaspi_rank_t), pointer :: array(:), rcv_array(:), src_array(:)
- integer(gaspi_notification_id_t) :: data_available
+ integer(gaspi_segment_id_t), parameter :: segment_id_src=0, segment_id_rcv=1
+ integer(gaspi_size_t), parameter :: segment_size=VLEN*sizeof(nProc), write_size=VLEN*sizeof(nProc)
+ integer(gaspi_rank_t), allocatable, target :: array_rcv(:), array_src(:)
+ integer(gaspi_notification_id_t) :: data_available, rcv_notf
+ integer(gaspi_notification_t) :: notf, id, val, rcv_val
  integer(gaspi_queue_id_t) :: queue_id = 0
  integer(gaspi_offset_t) :: loc_off = 0
  integer(gaspi_offset_t) :: rem_off = 0
+ integer(gaspi_memory_description_t) :: mdesc_src=0, mdesc_rcv=0
  integer(gaspi_notification_t) :: expected
- type(c_ptr) :: seg_ptr
- CALL MPI_INIT(ierr)
+ type(c_ptr) :: seg_ptr_rcv, seg_ptr_src
+ call MPI_INIT(ierr)
  
  res = gaspi_proc_init(GASPI_BLOCK)
+ call stop_on_error(res,"gaspi_proc_init")
  res = gaspi_proc_rank(iProc)
+ call stop_on_error(res,"gaspi_proc_rank")
  res = gaspi_proc_num(nProc)
- ! create segment
+ call stop_on_error(res,"gaspi_proc_num")
+ ! create arrays
+ allocate( array_rcv(1:VLEN), array_src(1:VLEN))
+ ! map segment to array
  segment_alloc_policy=GASPI_MEM_UNINITIALIZED
- res = gaspi_segment_create(int(segment_id,1), segment_size, GASPI_GROUP_ALL, GASPI_BLOCK, segment_alloc_policy)  
+ res = gaspi_segment_use(int(segment_id_src,1), c_loc(array_src), segment_size, &
+                         GASPI_GROUP_ALL, GASPI_BLOCK, mdesc_src)  
+ call stop_on_error(res,"gaspi_segment_use_1")
+ res = gaspi_segment_use(int(segment_id_rcv,1), c_loc(array_rcv), write_size, &
+                         GASPI_GROUP_ALL, GASPI_BLOCK, mdesc_rcv)
+ call stop_on_error(res,"gaspi_segment_use_2")
  ! get segment pointer
- res = gaspi_segment_ptr(int(segment_id,1), seg_ptr)
- ! convert c pointer to Fortran pointer
- call c_f_pointer(seg_ptr, array, shape=[segment_size/sizeof(iProc)])
+ res = gaspi_segment_ptr(int(segment_id_src,1), seg_ptr_src)
+ call stop_on_error(res,"gaspi_segment_ptr_1")
+ res = gaspi_segment_ptr(int(segment_id_rcv,1), seg_ptr_rcv)
+ call stop_on_error(res,"gaspi_segment_ptr_2")
+ ! put values in array
+ do j=1,VLEN
+  array_src(j) =  iProc*VLEN + j
+  print *,"Rank ",iProc," src elem ",j," : ",array_src(j)
+ enddo
+
  ! sync
  res = gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK)
+ call stop_on_error(res,"gaspi_barrier")
+ 
+ ! Uncomment line below and line 182 to use multiple queues, comment out 183 and
+ ! 184
+ !call wait_for_queue_entries_for_write_notify(queue_id)
 
- ! put values in array
- do j=0,VLEN-1
-  array(j) =  iProc*VLEN+j
- end do
-
- call wait_for_queue_entries_for_write_notify(queue_id)
+ ! right processor
  rProc = modulo(iProc+nProc+1,nProc)
- !right(iProc,nProc)
- res = gaspi_write_notify(segment_id, loc_off, rProc, &
-                          segment_id, rem_off, write_size, &
-                          data_available, 1 + iProc, &
+ notf = 1 + iProc
+ res = gaspi_write_notify(segment_id_src, loc_off, rProc, &
+                          segment_id_rcv, rem_off, write_size, &
+                          data_available, notf, &
                           queue_id, GASPI_BLOCK)
+ call stop_on_error(res,"gaspi_write_notify")
+ ! left processor
+ lProc = modulo(iProc+nProc-1,nProc) 
+ expected = 1 + lProc
+ call wait_or_die(segment_id_rcv, data_available, expected) 
 
- lProc = modulo(iProc+nProc+1,nProc) 
- !1+left(iProc,nProc)
- expected = lProc
- call wait_or_die(segment_id, data_available, expected) 
-
- do j = 0,VLEN-1
-  print *,"Rank ",iProc," rcv elem ",j," : ",rcv_array(j)
+ do j = 1,VLEN
+  print *,"Rank ",iProc," rcv elem ",j," : ",array_rcv(j)
  end do
 
- call wait_for_flush_queues()
-
+ !call wait_for_flush_queues()
+ res = gaspi_wait(queue_id, GASPI_BLOCK)
+ call stop_on_error(res,"gaspi_wait")
  res = gaspi_proc_term(GASPI_BLOCK)
-
+ call stop_on_error(res,"gaspi_proc_term")
+ deallocate(array_rcv,array_src)
  call MPI_FINALIZE(ierr)
 
 
